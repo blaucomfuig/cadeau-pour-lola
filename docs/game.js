497a922bb1729f41602ec1dc2530718f927ac606
blaucomfuig <sergio.corcuera01@estudiant.upf.edu>
2020-12-25 23:40:34 +0100

    ajouter des vraies questions

--- docs/game.js
+++ docs/game.js
@@ -16,7 +16,7 @@ let availableQuestions = []
 
 let questions = [
     {
-        question: "comme s'appele le premier copain de la tête de mila ?",
+        question: "comment s'appelle le premier copain de la tête de mila ?",
         choice1: 'Pilou',
         choice2: 'Monstre Bleu',
         choice3: 'Alim',
@@ -24,12 +24,12 @@ let questions = [
         answer: 4,
     },
     {
-        question: "Ton film preferé ?",
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '15',
-        answer: 2,
+        question: "c'est quoi le film préferé de sophie?",
+        choice1: 'la belle verte',
+        choice2: 'juste la fin du monde',
+        choice3: 'le nom de gens',
+        choice4: "l'écume des jours",
+        answer: 3,
     },
     {
         question: "de quoi mila a le plus peur ?",
@@ -40,12 +40,44 @@ let questions = [
         answer: 2,
     },
     {
-        question: "Ton film preferé ?",
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '15',
-        answer: 2,
+        question: "boris appelle liv comment ?",
+        choice1: 'Lise',
+        choice2: 'Ivy',
+        choice3: 'Lisie',
+        choice4: 'Yves',
+        answer: 4,
+    },
+    {
+        question: "c'est quoi ce qu'ils disent au restaurant l'alibi quand il n'y a plus de papier WC ?",
+        choice1: "thomas, vas-y en recharger !",
+        choice2: "il n'y a plus de papier WC",
+        choice3: 'il faut se débrouiller !',
+        choice4: "il n'y a plus de papel",
+        answer: 4,
+    },
+    {
+        question: "c'était qui le personnage préferé de bd de félix auquel il s'identifiait ?",
+        choice1: 'rahan',
+        choice2: 'simba',
+        choice3: 'rohani',
+        choice4: 'mafalda',
+        answer: 1,
+    },
+    {
+        question: "'aputi', ça veut dire quoi pour les inuits ?",
+        choice1: 'la neige qui tombe',
+        choice2: "la neige qui sert à faire de l'eau",
+        choice3: 'la neige sur le sol',
+        choice4: 'la neige douce',
+        answer: 3,
+    },
+    {
+        question: "de quoi miloud a le plus peur ?",
+        choice1: 'des souris',
+        choice2: 'du sable',
+        choice3: 'de la mort',
+        choice4: 'des toilettes publiques ',
+        answer: 1,
     },
     {
         question: "Ton film preferé ?",
@@ -56,52 +88,28 @@ let questions = [
         answer: 2,
     },
     {
-        question: "Ton film preferé ?",
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '15',
-        answer: 2,
-    },
-    {
-        question: "Ton film preferé ?",
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '15',
-        answer: 2,
-    },
-    {
-        question: "Ton film preferé ?",
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '15',
-        answer: 2,
-    },
-    {
-        question: "Ton film preferé ?",
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '15',
-        answer: 2,
-    },
-    {
-        question: "Ton film preferé ?",
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '15',
-        answer: 2,
+        question: "c'est quoi le film préferé de félix ?",
+        choice1: 'urga',
+        choice2: 'humans (vol. 1)',
+        choice3: 'danse avec les loups',
+        choice4: 'into the wild',
+        answer: 3,
     },
     {
         question: "je suis de quel couleur en ce moment ?",
         choice1: 'bleu',
-        choice2: 'taronge',
+        choice2: 'taronge barcelonais',
         choice3: 'rouge rouge rouge !',
         choice4: 'vert',
         answer: 3,
+    },
+    {
+        question: "c'était quoi le rôle de félix quand il dansait quand il était petit ?",
+        choice1: 'le tigre',
+        choice2: 'le pêcheur',
+        choice3: 'le poisson',
+        choice4: 'le berger',
+        answer: 2,
     }
 ]
 
@@ -120,7 +128,7 @@ getNewQuestion = () => {
         localStorage.setItem('mostRecentScore', score)
 
         return window.location.assign('end-win.html')
-    } else if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS && score !== 11){
+    } else if(availableQuestions.length === 1 || questionCounter > MAX_QUESTIONS && score !== 11){
         localStorage.setItem('mostRecentScore', score)
 
         return window.location.assign('end-lose.html')
@@ -182,7 +190,7 @@ choices.forEach(choice => {
         setTimeout(() =>{
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
-        }, 700)
+        }, 1200)
     })
 })
 
