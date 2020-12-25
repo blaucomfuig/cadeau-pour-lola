11862fe8ab3bc1b98de70d20172dca06973d01ae
blaucomfuig <sergio.corcuera01@estudiant.upf.edu>
2020-12-25 12:29:35 +0100

    add scroll first page

---
+++ docs/scrollfirst.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal('h1', {
+    delay: 400,
+    duration: 1200,
+    origin: 'top',
+    distance: '100px',
+    easing: 'ease',
+})
+
+ScrollReveal().reveal('.btn', {
+    delay: 1600,
+    duration: 1000,
+    easing: 'ease',
+})
